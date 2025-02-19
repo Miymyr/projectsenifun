573484e4e0e992d1dddf2a05c383ce72a8e9de5c
Miymyrr <novikovaarina070@gmil.com>
2025-02-20 01:17:08 +0300

    Added 50% page of history

--- app.py
+++ app.py
@@ -59,6 +59,54 @@ def history3():
 def history4():
     return render_template("untitled-4.html")
 
+@app.route('/history5')
+def history5():
+    return render_template("untitled-5.html")
+
+@app.route('/history51')
+def history51():
+    return render_template("untitled-5-1.html")
+
+@app.route('/history6')
+def history6():
+    return render_template("untitled-6.html")
+
+@app.route('/history61')
+def history61():
+    return render_template("untitled-6-1.html")
+
+@app.route('/history62')
+def history62():
+    return render_template("untitled-6-2.html")
+
+@app.route('/history621')
+def history621():
+    return render_template("untitled-6-2-1.html")
+
+@app.route('/history7')
+def history7():
+    return render_template("untitled-7.html")
+
+@app.route('/history71')
+def history71():
+    return render_template("untitled-7-1.html")
+
+@app.route('/history72')
+def history72():
+    return render_template("untitled-7-2.html")
+
+@app.route('/history721')
+def history721():
+    return render_template("untitled-7-2-1.html")
+
+@app.route('/history8')
+def history8():
+    return render_template("untitled-8.html")
+
+
+
+
+
 
 
 
